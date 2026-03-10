a920391ab4c843a1803ff62a488ef0948afdb9a3
Matart930 <114448978+Matart930@users.noreply.github.com>
2026-03-10 13:05:16 -0700

    Update roll_xwoba_compare.R
    Added ability to see ARIMA projected future values of Rolling xwOBA to see which players are trending up, down, or stabilizing.

--- roll_xwoba_compare.R
+++ roll_xwoba_compare.R
@@ -24,6 +24,20 @@ ui <- fluidPage(
         "Select Players:",
         choices = names(players),
         selected = "Shohei Ohtani"
+      ),
+      conditionalPanel(
+        
+        condition = "input.players.length == 1",
+        
+        sliderInput("forecast_n",
+                    "Forecast Future Plate Appearances:",
+                    value = 20,
+                    min = 5,
+                    max = 100,
+                    step = 5),
+        checkboxInput("show_forecast",
+                      "Show Forecast",
+                      value = TRUE)
       )
     ),
     
@@ -33,9 +47,20 @@ ui <- fluidPage(
   )
 )
 
+
 server <- function(input, output, session){
   
+  observe({
+    if(length(input$players) != 1) {
+      updateCheckboxInput(session, "show_forecast", value = FALSE)
+    }
+  })
+  
   output$rollingPlot <- renderPlotly({
+    
+    if (isTRUE(input$show_forecast) && length(input$players) != 1) {
+      showNotification("Select exactly one player to display a forecast.", type = "warning")
+    }
     
     req(input$players)
     req(input$pa_window)
@@ -49,7 +74,7 @@ server <- function(input, output, session){
     # Compute rolling xwOBA per player
     rolling_data <- filtered_data %>%
       group_by(player_name) %>%
-      arrange(game_date) %>%
+      arrange(game_date, game_pk, at_bat_number) %>%
       group_modify(~ rollxwOBA(.x, window_size)) %>%
       ungroup()
     
@@ -67,20 +92,32 @@ server <- function(input, output, session){
     
     # Add formatted date for tooltip
     rolling_data <- rolling_data %>%
-      mutate(date_label = paste(format(date, "%B"), ordinal_day(date)),
-             xwoba_label = sub("^0", "", sprintf("%.3f", rolling_xwoba)))
+      mutate(
+        date_label = paste(format(date, "%B"), ordinal_day(date)),
+        xwoba_label = sub("^0", "", sprintf("%.3f", rolling_xwoba))
+      )
     
     # Base ggplot lines
-    p <- ggplot(rolling_data,
-                aes(x = pa, y = rolling_xwoba, color = player_name, group = player_name,
-                    text = paste0("<b>", player_name, "</b>",
-                                  "<br>xwOBA: ", xwoba_label,
-                                  "<br>Last PA: ", date_label)
-                )) +
-      geom_line(size = 1) +
+    p <- ggplot(
+      rolling_data,
+      aes(
+        x = pa,
+        y = rolling_xwoba,
+        color = player_name,
+        group = player_name,
+        text = paste0(
+          "<b>", player_name, "</b>",
+          "<br>xwOBA: ", xwoba_label,
+          "<br>Last PA: ", date_label
+        )
+      )
+    ) +
+      geom_line(linewidth = 1) +
       geom_hline(yintercept = y_breaks, linetype = "dashed", color = "gray") +
-      scale_y_continuous(breaks = y_breaks,
-                         labels = function(x) sub("^0", "", sprintf("%.3f", x))) +
+      scale_y_continuous(
+        breaks = y_breaks,
+        labels = function(x) sub("^0", "", sprintf("%.3f", x))
+      ) +
       coord_cartesian(ylim = c(0.1, 0.6)) +
       theme_minimal() +
       theme(
@@ -89,20 +126,72 @@ server <- function(input, output, session){
         axis.text.x = element_blank(),
         axis.ticks.x = element_blank()
       ) +
-      labs(x = NULL, y = NULL,
-           color = "Player Key",
-           title = paste0("Rolling xwOBA (", window_size, " PAs)"))
+      labs(
+        x = NULL,
+        y = NULL,
+        color = "Player Key",
+        title = paste0("Rolling xwOBA (", window_size, " PAs)")
+      )
     
-   
-    # Convert to ggplotly with hover and dark tooltip
+    # Forecast only if exactly one player is selected
+    if (isTRUE(input$show_forecast) && length(input$players) == 1) {
+      
+      player_df <- rolling_data %>%
+        filter(player_name == input$players[1]) %>%
+        arrange(pa)
+      
+      req(nrow(player_df) > 10)
+      
+      fc <- forecast_xwoba(
+        xwoba_series = player_df$rolling_xwoba,
+        last_pa = max(player_df$pa),
+        n_future = input$forecast_n
+      )
+      
+      fc <- fc %>%
+        mutate(
+          future_pa = row_number(),
+          forecast_label = sub("^0", "", sprintf("%.3f", forecast)),
+          text = paste0(
+            "Projected xwOBA: ", forecast_label,
+            "<br>after ", future_pa, " future PAs"
+          )
+        )
+      
+      p <- p +
+        geom_line(
+          data = fc,
+          aes(x = pa, y = forecast, text = text),
+          inherit.aes = FALSE,
+          color = "red",
+          linetype = "dashed",
+          linewidth = 1
+        ) +
+        geom_point(
+          data = fc,
+          aes(x = pa, y = forecast, text = text),
+          inherit.aes = FALSE,
+          color = "red",
+          size = 1.8,
+          alpha = 0.8
+        ) +
+        geom_ribbon(
+          data = fc,
+          aes(x = pa, ymin = lower, ymax = upper),
+          inherit.aes = FALSE,
+          alpha = 0.2,
+          fill = "red"
+        )
+    }
+    
     ggplotly(p, tooltip = "text", source = "rolling_plot") %>%
       layout(
         hoverlabel = list(font = list(color = "white"))
-      ) 
+      )
   })
-  
 }
 
 shinyApp(ui, server)
+
 
 
